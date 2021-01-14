8d12bd0d5726bcdbbc51ec6fc191526ac524db6e
liuji <liuji@163.com>
2021-01-14 11:01:28 +0800

    add some information

--- src/main/java/org/fdg/demo/FdgDemo.java
+++ src/main/java/org/fdg/demo/FdgDemo.java
@@ -1,6 +1,14 @@
 package org.fdg.demo;
 
+import com.github.fdg.FDGRawTransaction;
+import com.github.fdg.Numeric;
+import com.github.model.BlockResult;
 import com.github.rpc.RpcClient;
+import com.github.model.TransactionRecepit;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.crypto.TransactionEncoder;
+import org.web3j.utils.Convert;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -29,30 +37,50 @@ public class FdgDemo {
     /***
      * 获取地址的交易笔数
      */
-    public void getTradeCount(){
+    public BigInteger getTradeCount(String from ){
         RpcClient client= FdgInstance.getInstance();
-        BigInteger integer=client.getTradeCount("Gs950d02552fCce39B6eF23Bc15081DA6F0ab00132");
+        BigInteger integer=client.getTradeCount(from);
         System.out.println(integer);
+        return integer;
     }
 
 
     /**
-     * eth发送交易
+     *
+     * @param privateKey  用户私钥
+     * @param to    发送地址
      */
-    public void sendTrade(){
+    public void sendTrade(String privateKey,String to){
         RpcClient client= FdgInstance.getInstance();
-        String tradeHash=client.sendRawTransaction("Gs950d02552fCce39B6eF23Bc15081DA6F0ab00132");
+        Credentials credentials = Credentials.create(privateKey);
+        BigInteger nonce = getTradeCount("from");
+        BigInteger gasPrice= Convert.toWei("45", Convert.Unit.GWEI).toBigInteger();
+        RawTransaction rawTransaction = FDGRawTransaction.createEtherTransaction(nonce, gasPrice, new BigInteger("60000"), to, null);
+        byte[] signMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+        String hexValue = Numeric.toHexString(signMessage);
+        String tradeHash=client.sendRawTransaction(hexValue);
         System.out.println(tradeHash);
     }
 
     /**
-     *  获取地址交易笔数
+     *  根据hash获取交易信息
      */
-    public void syncTrade(){
+    public void getTradeByTxHash(){
         RpcClient client= FdgInstance.getInstance();
-        BigInteger count=client.getTradeCount("Gs950d02552fCce39B6eF23Bc15081DA6F0ab00132");
-        System.out.println(count);
+        TransactionRecepit receipt =client.getTransactionReceipt("xxx"); //写入交易hash
+        System.out.println(receipt);
     }
+
+    /**
+     * 获取指定区块
+     * @param start
+     */
+     public void getBlockInfoByNumber(Long start){
+         RpcClient client= FdgInstance.getInstance();
+         BlockResult blockResult= client.getBlock(start,false);
+     }
+
+
 
     public static void main(String[] args) {
         FdgDemo demo=new FdgDemo();
