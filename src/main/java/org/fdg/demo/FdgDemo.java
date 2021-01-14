77e0bd0c6f8002457fdaea6057fd8c75604e4e81
liuji <liuji@163.com>
2021-01-14 13:28:46 +0800

    update

--- src/main/java/org/fdg/demo/FdgDemo.java
+++ src/main/java/org/fdg/demo/FdgDemo.java
@@ -5,13 +5,30 @@ import com.github.fdg.Numeric;
 import com.github.model.BlockResult;
 import com.github.rpc.RpcClient;
 import com.github.model.TransactionRecepit;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.RawTransaction;
 import org.web3j.crypto.TransactionEncoder;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.admin.Admin;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.Contract;
 import org.web3j.utils.Convert;
+import sun.security.pkcs11.wrapper.Constants;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class FdgDemo {
 
@@ -80,12 +97,81 @@ public class FdgDemo {
          BlockResult blockResult= client.getBlock(start,false);
      }
 
+    /**
+     * 代币交易
+     */
+    public void sendTrade(String privateKey,String from ,String contractAddress,String to,BigInteger value){
+         Credentials credentials = Credentials.create(privateKey);
+         //获取nonce，交易笔数
+         BigInteger nonce = getTradeCount(from);
+         //get gasPrice
+        BigInteger gasPrice= Convert.toWei("45", Convert.Unit.GWEI).toBigInteger();
+
+
+         //创建RawTransaction交易对象
+         Function function = tranFunction(to, value);
+
+         String encodedFunction = FunctionEncoder.encode(function);
+
+
+         RawTransaction rawTransaction = FDGRawTransaction.createTransaction(nonce, gasPrice, new BigInteger("60000"), to, encodedFunction);
+
+         //签名Transaction，这里要对交易做签名
+         byte[] signMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+
+         String hexValue = Numeric.toHexString(signMessage);
+         //发送交易
+        RpcClient client= FdgInstance.getInstance();
+        String tradeHash=client.sendRawTransaction(hexValue);
+        System.out.println(tradeHash);
+
+
+
+     }
+    private Function tranFunction(String to, BigInteger value) {
+        Function function = null;
+        try {
+             function = new Function(
+                    "",
+                    Arrays.asList(new Address(to), new Uint256(value)),
+                    Collections.singletonList(new TypeReference<Type>() {
+                    }));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return function;
+
+    }
+
+    private static final String DATA_PREFIX = "0x70a08231000000000000000000000000";
+
+    /**
+     * 获取合约地址的余额
+     * @param address
+     * @param contractAddress
+     * @return
+     * @throws Exception
+     */
+    public static BigDecimal getBalance(String address, String contractAddress)  {
+        System.out.println(address.substring(2));
+        Transaction transaction =FDGRawTransaction.createEthCallTransaction(address, contractAddress, DATA_PREFIX + address.substring(2) );
+
+        RpcClient client= FdgInstance.getInstance();
+        BigDecimal value = client.getBalance(transaction, DefaultBlockParameterName.PENDING);
+
+        System.out.println(value);
+
+        return value;
+    }
 
 
     public static void main(String[] args) {
         FdgDemo demo=new FdgDemo();
-        demo.getBlock();
+        demo.getBalance("Gsabff5fb38e4ed2da3009c6b97a368ea2fb58f3c5","Gsdac17f958d2ee523a2206206994597c13d831ec7");
     }
+
+
 
 
 
